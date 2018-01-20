c162461de651c8c847266e225258c495d63c955c
Melanie Brownfield <melanie.brownfield1@gmail.com>
2018-01-20 05:13:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,5 @@ function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
 function happyHolidayTo(holiday, name){
-  return 'Happy ${Independence Day}, ${Melanie}!'
+  return `Happy ${holiday}, ${name}!`;
 }
