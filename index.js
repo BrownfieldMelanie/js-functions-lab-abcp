ad6d56d32c4999cc5281c2fe3bb64c20c46a3c3e
Melanie Brownfield <melanie.brownfield1@gmail.com>
2018-01-20 04:27:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // write your code below!
 function happyHolidays(){
- console.log ("Happy holidays!")
- return "Happy holidays!"
+ console.log ("Happy holidays!");
+ return "Happy holidays!";
   }
   
+function happyHolidaysTo(name){
+  console.log('Happy holidays, ${name}!');
+  return 'Happy holidays, ${name}';
+}
